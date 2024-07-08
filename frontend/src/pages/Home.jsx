750d88c0f5aa447f684aa7556996f1d5f0498dbc
harshitakumai-dev <harshitakumari344@gmail.com>
2024-07-08 20:08:28 +0530

    completed event section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,31 +1,61 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState } from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-import Slider from '../assets/Slider.png'
+import { Swiper, SwiperSlide } from "swiper/react";
+
 // Import Swiper styles
-import 'swiper/css';
-
-
+import "swiper/css";
 
 // import required modules
-import { Autoplay} from 'swiper/modules';
+import { Autoplay } from "swiper/modules";
 
-
-import Stripes from '../components/Stripes';
-import About from '../components/About';
-import Gallery from '../components/Gallery';
-import Divider from '../components/Divider';
-import Services from '../components/Services';
-import Slider from '../components/Slider';
-
-
+import Stripes from "../components/Stripes";
+import About from "../components/About";
+import Gallery from "../components/Gallery";
+import Divider from "../components/Divider";
+import Services from "../components/Services";
+import VenueFilter from "../components/EventSection";
 
 const Home = () => {
   return (
     <div>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        loop={"true"}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Autoplay]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div>
+            <img src="/slider.png" alt="" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>
+          <img src="/slider.png" alt="" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>
+          <img src="/slider.png" alt="" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>
+          <img src="/slider.png" alt="" />
+          </div>
+        </SwiperSlide>
+      </Swiper>
 
       {/*image Slider */}
-      <Slider/>
+   
 
       {/* Stripes */}
 
@@ -35,22 +65,18 @@ const Home = () => {
       <About />
 
       {/* Gallery */}
-     
 
       {/* Divider */}
       <Divider />
 
       {/* Services */}
-      <Services/>
-   
-      <EventSection/>
-   
+      <Services />
+
+      <VenueFilter />
 
       <Stripes alignment={`items-start`} />
-
-      
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
